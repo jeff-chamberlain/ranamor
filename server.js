49d4e522a23467c5ac596c7b40d7b36fa2be52fc
Jeff Chamberlain <jcchamberlain@gmail.com>
2013-10-02 09:10:58 -0700

    db test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var io = require('socket.io'),
   express = require('express'),
   http = require('http'),
   ran = require('ranamor-mod');
+  mysql = require('mysql');
   
 var app = express();
 var server = http.createServer(app);
@@ -12,6 +13,15 @@ ranio.configure(function () {
   ranio.set("transports", ["xhr-polling"]);
   ranio.set("polling duration", 10); 
 });
+
+var pool  = mysql.createPool({
+  host     : 'mysql.exninja.com',
+  user     : 'exninja',
+  password : 'ex1stent1al',
+  database : 'ranadb',
+});
+
+ran.config_db(pool);
 
 ranio.of('/player').on('connection', function(socket) {
 	ran.config_player({
